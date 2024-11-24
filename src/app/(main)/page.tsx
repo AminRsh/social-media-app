bbd9449aeafdf181fe69c8558cbb8fb7a8c12a45
AminRsh <amin.sharifi45@gmail.com>
2024-11-24 12:56:59 -0500

    following-feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,8 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
-import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsTrigger, TabsList } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 
 export default function Home() {
@@ -12,9 +11,16 @@ export default function Home() {
     <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+            <TabsList>
+              <TabsTrigger value="for-you">For You</TabsTrigger>
+              <TabsTrigger value="following">Following</TabsTrigger>
+            </TabsList>
+          <TabsContent value="for-you"><ForYouFeed /></TabsContent>
+          <TabsContent value="following"><FollowingFeed /></TabsContent>
+        </Tabs>
       </div>
-      <TrendsSidebar />    
+      <TrendsSidebar />
     </main>
   );
 }
